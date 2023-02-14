b0a282a782e3e1d62ff997af54b62d723b4d8db0
Andrea Vaira <andrea.vaira@cambieri.it>
2023-02-14 11:10:17 +0100

    Recipe and shopping list model created

---
+++ src/app/Models/ingredient.model.ts
@@ -0,0 +1,10 @@
+export class IngredientModel{
+  public _id:string = '';
+  public name:string;
+  public amount:number;
+
+  constructor(name:string, amount:number) {
+    this.name = name;
+    this.amount = amount;
+  }
+}
