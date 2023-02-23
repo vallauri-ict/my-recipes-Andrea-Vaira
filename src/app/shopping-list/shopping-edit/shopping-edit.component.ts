c4fa3dd74ea2aa062a542527163e15de0bca3a5b
Andrea Vaira <andrea.vaira@cambieri.it>
2023-02-23 09:41:04 +0100

    Shopping list management done

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output , ViewChild , ElementRef} from '@angular/core';
+import { IngredientModel } from 'src/app/Models/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent {
+  @ViewChild('name') _txtName!: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<IngredientModel>();
+  ingredientName: string = '';
+  ingredientAmount: number = 0;
 
+  addIngredient() {
+    if (this.ingredientAmount > 0) {
+      const myIngredient: IngredientModel = new IngredientModel(
+        this.ingredientName,
+        this.ingredientAmount
+      );
+      this.ingredientAdded.emit(myIngredient);
+    } else {
+      alert('Inserire una quantità maggiore di 0');
+    }
+  }
+
+  clearFields() {
+    this.ingredientName = '';
+    this.ingredientAmount = 0;
+    this._txtName.nativeElement.focus();
+  }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -12,4 +12,15 @@ export class ShoppingListComponent {
     new IngredientModel("pomodori", 10),
     new IngredientModel("spinaci", 7)
   ]
+
+  onIngredientAdded(myIngredient:IngredientModel){
+    for (const item of this.ingredients) {
+      if (item.name.toLowerCase() == myIngredient.name.toLowerCase()) {
+        item.amount += myIngredient.amount;
+        return;
+      }
+    }
+    this.ingredients.push(myIngredient);
+
+  }
 }
